76c1e0db43d7e8d18d373670bd2711fa3b0fa642
Eber David <ebrych@gmail.com>
2019-05-14 17:53:20 -0500

    1º753

---
+++ SEMAPP.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "8a517f81-d25e-4bc9-8cc5-40018b5fb53f",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
